cac83959dd10b1662316ef49d30f68560c9f69f7
syedtalaljilani <talaljilani79@gmail.com>
2022-08-07 15:05:07 +0500

    commit

--- src/views/EmployeeView.vue
+++ src/views/EmployeeView.vue
@@ -2,15 +2,18 @@
   <div>
     <v-container>
       <h1 class="text-center">My Team</h1>
-      <v-row>
-        <v-card>
-          <v-card-actions>
-            <v-avatar
-              size="100"
-              src="../assets/logo.png"></v-avatar>
-          </v-card-actions>
-        </v-card>
-      </v-row>
+      <v-card>
+        <v-card-text>
+          <v-row justify="center"> 
+            <v-avatar size="100" rounded>
+              <v-icon size="50">mdi-email</v-icon>
+            </v-avatar>
+            </v-row>
+        </v-card-text>
+        <v-card-text>
+          <v-row justify="center"> profile </v-row>
+        </v-card-text>
+      </v-card>
     </v-container>
   </div>
 </template>
